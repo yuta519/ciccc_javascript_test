e37eda94764a078969c9ea92a7a02fa3187867d2
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-02-04 13:08:06 +0900

    add no8

---
+++ No8.js
@@ -0,0 +1,17 @@
+// ###### 8- Convert this code to arrow function.
+// 
+// ```js
+// function greeting(firstname, lastname) {
+//   return `Hello ${firstname} ${lastname}`;
+// }
+// // after this line,
+// 
+// ```
+
+const greeting = (firstname, lastname) => {
+  return `Hello ${firstname} ${lastname}`;
+}
+
+const message = greeting('Yuta', 'Kawamura')
+console.log(message)
+
